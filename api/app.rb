bc4c7f534ee7b9b7209b74486b132141cbea516d
Nicolas Agathos <nicolas.agathos@gmail.com>
2022-04-05 00:47:24 +0200

    base skelleton

---
+++ api/app.rb
@@ -0,0 +1,18 @@
+post 'v1/indexations' do
+  # TK: To Do
+end
+
+configure do
+  enable :cross_origin
+end
+
+before do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+end
+
+options '*' do
+  response.headers['Allow'] = 'GET, PUT, POST, DELETE, OPTIONS'
+  response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token'
+  response.headers['Access-Control-Allow-Origin'] = '*'
+  200
+end
