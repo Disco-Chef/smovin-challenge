a68e5e1152bc3417378fe6e76e1b9b91f13472a4
Nicolas Agathos <nicolas.agathos@gmail.com>
2022-04-05 01:57:44 +0200

    layout the api order of operations

--- api/app.rb
+++ api/app.rb
@@ -1,6 +1,14 @@
+require 'sinatra'
+
 post 'v1/indexations' do
-  # TK: To Do
-end
+  # TK: Parse body into Date instances etc
+
+  # TK: Check for bad params. If so, respond with status 400 with errors for each input in the response body
+
+  # TK: Else, use objects from parsed body to do the calls to the indexation endpoint => Indexator.new.call(region, rent, signed_on, start_on)
+
+  # TK: Respond with status 200 and response body with the new_rent, base_index, current_index
+en
 
 configure do
   enable :cross_origin
