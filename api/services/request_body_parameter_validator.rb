ec33a7c555b0c841b401d7d24ec6374430657f31
Nicolas Agathos <nicolas.agathos@gmail.com>
2022-04-05 05:36:56 +0200

    drier date validation

--- api/services/request_body_parameter_validator.rb
+++ api/services/request_body_parameter_validator.rb
@@ -17,8 +17,7 @@ class RequestBodyParameterValidator
   def run
     validate_region
     validate_rent
-    validate_signed_on
-    validate_start_date
+    validate_dates
   end
 
   private
@@ -40,32 +39,47 @@ class RequestBodyParameterValidator
   end
 
   # TK: Make DRY these date validators:
-  def validate_signed_on
-    unless @signed_on.nil?
-      begin
-        @signed_on = Date.parse(@signed_on)
-      rescue
-        @errors['signed_on'] = ['invalid_date']
-      else
-        @errors['signed_on'] = ['date_cannot_be_in_the_future'] if @signed_on > Date.today
-      end
-    else
-      @errors['signed_on'] = ['missing']
-    end
-  end
+  # def validate_signed_on
+  #   unless @signed_on.nil? || @signed_on.empty?
+  #     begin
+  #       @signed_on = Date.parse(@signed_on)
+  #     rescue
+  #       @errors['signed_on'] = ['invalid_date']
+  #     else
+  #       @errors['signed_on'] = ['date_cannot_be_in_the_future'] if @signed_on > Date.today
+  #     end
+  #   else
+  #     @errors['signed_on'] = ['missing']
+  #   end
+  # end
 
-  def validate_start_date
-    unless @start_date.nil?
-      begin
-        @start_date = Date.parse(@start_date)
-      rescue
-        @errors['start_date'] = ['invalid_date']
-      else
-        @errors['start_date'] = ['date_cannot_be_in_the_future'] if @start_date > Date.today
+  # def validate_start_date
+  #   unless @start_date.nil? || @start_date.empty?
+  #     begin
+  #       @start_date = Date.parse(@start_date)
+  #     rescue
+  #       @errors['start_date'] = ['invalid_date']
+  #     else
+  #       @errors['start_date'] = ['date_cannot_be_in_the_future'] if @start_date > Date.today
+  #     end
+  #   else
+  #     @errors['start_date'] = ['missing']
+  #   end
+  # end
 
+  def validate_dates
+    {'signed_on' => @signed_on, 'start_date' => @start_date}.each do |attr_name, date_string|
+      unless date_string.nil? || date_string.empty?
+        begin
+          date_object = Date.parse(date_string)
+        rescue
+          @errors[attr_name] = ['invalid_date']
+        else
+          @errors[attr_name] = ['date_cannot_be_in_the_future'] if date_object > Date.today
+        end
+      else
+        @errors[attr_name] = ['missing']
       end
-    else
-      @errors['start_date'] = ['missing']
     end
   end
 end
