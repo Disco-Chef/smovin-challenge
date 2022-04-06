113ba56a7c97b19d8fc682e8ccb7221ffb36edc0
Nicolas Agathos <nicolas.agathos@gmail.com>
2022-04-07 00:43:55 +0200

    clearer naming

--- api/spec/request_body_parameter_validator_spec.rb
+++ api/spec/request_body_parameter_validator_spec.rb
@@ -26,14 +26,14 @@ describe 'RequestBodyParameterValidator' do
       }
       validator = RequestBodyParameterValidator.new(bad_params)
 
-      expected_errors_bad_params = {
+      expected_errors_object_from_bad_params = {
         'region' => ['must_be_one_of_brussels_or_wallonia_or_flanders'],
         'base_rent' => ['must_be_positive'],
         'signed_on' => ['invalid_date'],
         'start_date' => ['cannot_be_in_the_future']
       }
 
-      expect(validator.errors).to eq(expected_errors_bad_params)
+      expect(validator.errors).to eq(expected_errors_object_from_bad_params)
     end
 
     it 'returns a hash with an start_date_and_signed_on_conflict key if the signed_on and start_date are valid but not sequential in that order' do
@@ -45,11 +45,11 @@ describe 'RequestBodyParameterValidator' do
       }
       validator = RequestBodyParameterValidator.new(bad_params)
 
-      expected_errors_bad_params = {
+      expected_errors_object_from_bad_params = {
         'start_date_and_signed_on_conflict' => ['start_date_must_be_in_the_future_of_signed_on_date']
       }
 
-      expect(validator.errors).to eq(expected_errors_bad_params)
+      expect(validator.errors).to eq(expected_errors_object_from_bad_params)
     end
   end
 end
